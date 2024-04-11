8fa81fdb7a9b561bf96d8e89e5a314ba2948db1a
Habib-0007 <adebayohabib7@gmail.com>
2024-04-11 12:38:39 +0100

    Setup the CORS so that api endpoint can vbe accessible on different networks

--- server.js
+++ server.js
@@ -1,26 +1,86 @@
+require("dotenv").config();
 const express = require("express");
 const fs = require("fs");
 const app = express();
 
-const PORT = 3000;
+const port = process.env.PORT || 5001;
 
-app.get("/data", (req, res) => {
-	fs.readFile("pickup_lines.json", "utf8", (err, data) => {
-		if (err) {
-			console.error(`Error reading file from disk: ${err}`);
-			res.status(500).send({ error: err.message });
-			return;
+app.use(express.json());
+
+app.get("/pickups", (req, res) => {
+	res.set(
+		"Access-Control-Allow-Origin",
+		"*"
+	);
+	fs.readFile(
+		"pickup_lines.json",
+		"utf8",
+		(err, data) => {
+			if (err) {
+				console.error(
+					`Error reading file from disk: ${err}`
+				);
+				res
+					.status(500)
+					.send({ error: err.message });
+				return;
+			}
+			try {
+				const parsedData =
+					JSON.parse(data);
+				res.send(parsedData);
+				console.log(parsedData);
+			} catch (err) {
+				console.error(
+					`Error parsing JSON string: ${err}`
+				);
+				res
+					.status(500)
+					.send({ error: err.message });
+			}
 		}
-		try {
-			const parsedData = JSON.parse(data);
-			res.send(parsedData);
-		} catch (err) {
-			console.error(`Error parsing JSON string: ${err}`);
-			res.status(500).send({ error: err.message });
-		}
-	});
+	);
 });
 
-app.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}`);
+app.get("/pickups/:id", (req, res) => {
+	res.set(
+		"Access-Control-Allow-Origin",
+		"*"
+	);
+	fs.readFile(
+		"pickup_lines.json",
+		"utf8",
+		(err, data) => {
+			if (err) {
+				console.error(
+					`Error reading file from disk: ${err}`
+				);
+				res
+					.status(500)
+					.send({ error: err.message });
+				return;
+			}
+			try {
+				const parsedData =
+					JSON.parse(data);
+				const newData = parsedData.find(
+					data => req.params.id == data.id
+				);
+				res.send(newData);
+			} catch (err) {
+				console.error(
+					`Error parsing JSON string: ${err}`
+				);
+				res
+					.status(500)
+					.send({ error: err.message });
+			}
+		}
+	);
+});
+
+app.listen(port, () => {
+	console.log(
+		`Server listening on port ${port}`
+	);
 });
